47a2e20204c5f8c20cf49366820a743f6cbeae69
LiuLang <gsushzhsosgsu@gmail.com>
2013-05-26 18:53:50 +0800

    gmeasure - transparent window

--- gmeasure/Handler.py
+++ gmeasure/Handler.py
@@ -1,3 +1,5 @@
+#from gi.repository import cairo
+import cairo
 from gi.repository import Gdk
 from gi.repository import Gtk
 
@@ -7,30 +9,34 @@ class Handler:
         self.builder = builder
 
         self.window = builder.get_object('main_window')
+        self.screen = self.window.get_screen()
+        self.gdk_window = self.screen.get_root_window()
+        visual = self.screen.get_rgba_visual()
+        if visual != None and self.screen.is_composited():
+            self.window.set_visual(visual)
 
     def run(self):
         self.window.show_all()
         Gtk.main()
 
     def on_app_exit(self, widget, event=None):
+        self._set_cursor(Gdk.CursorType.ARROW)
         Gtk.main_quit()
 
+    def on_main_window_draw(self, window, cr):
+        cr.set_source_rgba(0.2, 0.2, 0.2, 0.8)
+        cr.set_operator(cairo.OPERATOR_SOURCE)
+        cr.paint()
+
     def on_main_window_button_press_event(self, window, event):
-        #print('button pressed')
         label_menu = self.builder.get_object('label_menu')
         if event.type != Gdk.EventType.BUTTON_PRESS:
             return False
 
         if event.button == 3:
-            # right click
             label_menu.popup(None, None, None, None, 
                     event.button, event.time)
-        elif event.button == 2:
-            # middle clcick, current do nothing.
-            pass
         elif event.button == 1:
-            # left click, resize window.
-            # first, get window edge.
             move_or_resize, edge_type = self._check_move_or_resize(event)
             if move_or_resize == False:
                 self.window.begin_move_drag(event.button, event.x_root,
@@ -39,13 +45,10 @@ class Handler:
                 self.window.begin_resize_drag(edge_type, event.button, 
                         event.x_root, event.y_root, event.time)
 
-    def _check_move_or_resize(self, event, cursor=False):
-        cursors = {
-                '1': None,
-                '2': Gdk.CursorType.BOTTOM_SIDE,
-                '4': Gdk.CursorType.RIGHT_SIDE,
-                '8': Gdk.CursorType.BOTTOM_RIGHT_CORNER,
-                }
+    def _check_move_or_resize(self, event):
+        '''
+        Return True when need to resize, else False.
+        '''
         edges = {
                 '1': None,
                 '2': Gdk.WindowEdge.SOUTH,
@@ -59,37 +62,30 @@ class Handler:
             mark *= 4
         if height - MIN <= event.y <= height + MIN:
             mark *= 2
-
-        if mark == 1:
-            move_or_resize = False
-        else: 
-            move_or_resize = True
-        if cursor == False:
-            side = edges[str(mark)]
-        else:
-            side = cursors[str(mark)]
-        return (move_or_resize, side)
+        move_or_resize = mark != 1
+        return (move_or_resize, edges[str(mark)])
 
     def _set_cursor(self, cursor_type):
-        screen = self.window.get_screen()
-        gdk_window = screen.get_root_window()
         cursor = Gdk.Cursor(cursor_type)
-        gdk_window.set_cursor(cursor)
+        self.gdk_window.set_cursor(cursor)
 
     def on_main_window_enter_notify_event(self, window, event):
+        self.do_change_cursor(window, event)
+
+    def do_change_cursor(self, window, event):
         move_or_resize, edge_type = self._check_move_or_resize(event)
+        cursors = {
+                Gdk.WindowEdge.SOUTH: Gdk.CursorType.BOTTOM_SIDE,
+                Gdk.WindowEdge.EAST: Gdk.CursorType.RIGHT_SIDE,
+                Gdk.WindowEdge.SOUTH_EAST: Gdk.CursorType.BOTTOM_RIGHT_CORNER,
+                }
         if move_or_resize == True:
-            self._set_cursor(edge_type)
+            self._set_cursor(cursors[edge_type])
         else:
             self._set_cursor(Gdk.CursorType.HAND2)
 
     def on_main_window_motion_notify_event(self, window, event):
-        self.on_main_window_enter_notify_event(window, event)
+        self.do_change_cursor(window, event)
 
     def on_main_window_leave_notify_event(self, window, event):
-        move_or_resize, cursor_type = self._check_move_or_resize(event, 
-                cursor=True)
-        if move_or_resize == True:
-            self._set_cursor(cursor_type)
-        else:
-            self._set_cursor(Gdk.CursorType.ARROW)
+        self._set_cursor(Gdk.CursorType.ARROW)
